07d3698d0f426211ad6a16914d5074aa8c0eac0f
Michael Tse <Michaeltessey1@gmail.com>
2023-02-14 16:34:35 -0500

    updated a bit of landing page

--- src/content/Landing/Landing.js
+++ src/content/Landing/Landing.js
@@ -1,10 +1,15 @@
 import React from 'react';
+import { FaReact } from 'react-icons/fa';
 import './Landing.css';
 
 const Landing = () => {
   return (
     <div className='Landing'>
-      <div>THIS IS THE LANDING PAGE</div>
+      <div>Hi there, I'm</div> <br/>
+      <div>Tessey</div> <br/>
+      <div>Full Stack Developer</div> <br/>
+      <div>I am a programer from New York</div> <br/>
+      <div>and I absolutely love working with <FaReact size="20px" color="#61DAFB" /></div>
     </div>
   );
 };
