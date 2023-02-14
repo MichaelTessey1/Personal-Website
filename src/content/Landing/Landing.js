102299dd934095c69ca506ce27f3e6c418c17e03
MichaelTessey1 <michaeltessey1@gmail.com>
2023-02-14 18:36:11 -0500

    almost done styling landing page

--- src/content/Landing/Landing.js
+++ src/content/Landing/Landing.js
@@ -1,15 +1,20 @@
 import React from 'react';
 import { FaReact } from 'react-icons/fa';
 import './Landing.css';
+import Baji from '../Images/baji2.jpg'
 
 const Landing = () => {
   return (
     <div className='Landing'>
-      <div>Hi there, I'm</div> <br/>
-      <div>Tessey</div> <br/>
-      <div>Full Stack Developer</div> <br/>
-      <div>I am a programer from New York</div> <br/>
-      <div>and I absolutely love working with <FaReact size="20px" color="#61DAFB" /></div>
+      <div className = "wrap">
+        <div className = 'landing-container'>
+          <div className = "t1">Hi there, I'm Michael but also known as,</div> <br/>
+          <div className = "t2">Tessey</div> <br/>
+          <div className = "t3">I am a Full Stack Developer from New York</div> <br/>
+          <div className = "t4">and I absolutely love working with <FaReact size="20px" color="#61DAFB" /></div>
+        </div>
+        <img src={Baji} alt="anime character that looks like me" className = "baji"/>
+      </div>
     </div>
   );
 };
